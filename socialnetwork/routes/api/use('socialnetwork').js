63f4eb1a1cd5ca329ee8fa5ce2c6fdfa9f6fe963
Tanisha Barreau <tanisha.barreau101@gmail.com>
2023-07-24 08:03:53 -0400

    commiting

---
+++ socialnetwork/routes/api/use('socialnetwork').js
@@ -0,0 +1,7 @@
+use('socialnetwork')
+
+db.thoughts.insert({'thoughtText':'today is sunny'})
+
+db.thoughts.insert({'thoughtText' : 'I hope it dont rains'})
+
+db.thoughts.insert({'thoughtText' : 'I love my cats'})
